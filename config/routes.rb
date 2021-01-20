2659451599e280cb8e0ba5a10324693ae6b9344d
Richard <richard.wigley@gmail.com>
2021-01-20 17:57:52 +0000

    CH2 - Set the routes constraints for the API

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,7 @@
 Rails.application.routes.draw do
+  # API definition
+  namespace :api, defaults: { format: :json } do
+    # add resources here under controllers
+    # namespaced with api
+  end
 end
