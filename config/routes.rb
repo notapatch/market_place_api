8688d99abfdad2d4e6a8a0f6d24f338fa5443e03
Richard <richard.wigley@gmail.com>
2021-01-20 21:54:04 +0000

    CH3 - Adds the user create endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       # add resources here under controllers
       # namespaced with api
-      resources :users, only: [:show]
+      resources :users, only: %i[show create]
     end
   end
 end
