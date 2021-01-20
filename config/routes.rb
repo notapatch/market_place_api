76340d5b02591d3c13d1018c0af7ab681df34903
Richard <richard.wigley@gmail.com>
2021-01-20 22:04:09 +0000

    CH3 - Adds update action the users controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       # add resources here under controllers
       # namespaced with api
-      resources :users, only: %i[show create]
+      resources :users, only: %i[show create update]
     end
   end
 end
