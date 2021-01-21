ed092b6f98f71023bb4daa42749705e0c1d10724
Richard <richard.wigley@gmail.com>
2021-01-21 14:19:31 +0000

    Ch05 - Generate product model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_20_182533) do
+ActiveRecord::Schema.define(version: 2021_01_21_141112) do
+
+  create_table "products", force: :cascade do |t|
+    t.string "title"
+    t.decimal "price"
+    t.boolean "published"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -19,4 +29,5 @@ ActiveRecord::Schema.define(version: 2021_01_20_182533) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "products", "users"
 end
